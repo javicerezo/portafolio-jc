26518a6fe6f22853771b4b884d59eb46f64ebfc0
Javi Cerezo <jc.webmob@gmail.com>
2025-07-18 17:20:30 +0200

    Crear Hero y aplicar estilos

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
-import { Header } from "./components/Header/Header"
-import { Footer } from "./components/Footer/Footer"
-import { Contact } from "./components/Contact/Contact"
-import { Proyect } from "./components/Proyect/Proyect"
-import { AboutMe } from "./components/AboutMe/AboutMe"
+import { Header } from "./components/Header/Header";
+import { Footer } from "./components/Footer/Footer";
+import { Contact } from "./components/Contact/Contact";
+import { Proyect } from "./components/Proyect/Proyect";
+import { AboutMe } from "./components/AboutMe/AboutMe";
+import { Hero } from "./components/Hero/Hero";
 
 function App() {
   return (
     <>
       <Header />
+      <Hero />
       <Proyect />
       <AboutMe />
       <Contact />
---
+++ src/components/Hero/Hero.tsx
@@ -0,0 +1,52 @@
+import { SiHtml5, SiCss3, SiSass, SiJavascript, SiTypescript,
+         SiReact, SiVite, SiNodedotjs, SiPostgresql, SiFirebase, SiAndroidstudio,
+         SiKotlin } from 'react-icons/si';
+import { FaJava, FaGithub  } from "react-icons/fa";
+import { MdOutlineWeb } from "react-icons/md";
+import { FaLinkedin } from "react-icons/fa6";
+export const Hero = () => {
+    const web = "${ web }";
+    const mobile = "${ móvil }";
+
+    return (
+        <section className='Hero'>
+            <div className="Hero-container">
+                <div className='Hero-up'>
+                    <h1 className='Hero-h1'>Javi Cerezo</h1>
+                    <div className="Hero-div">
+                        <h2 className='Hero-h2 Hero-h2--first'>Desarrollador:</h2> 
+                        <div className='Hero-props'>
+                            <h2 className='Hero-h2 Hero-h2--second'>prop1 = {web}</h2> 
+                            <h2 className='Hero-h2 Hero-h2--last'>prop2 = {mobile}</h2>
+                        </div>
+                    </div>
+                    <ul className="Hero-ul">
+                        <li className="Hero-li"><SiHtml5 color='#E34F26'/></li>
+                        <li className="Hero-li"><SiCss3 color='#663399'/></li>
+                        <li className="Hero-li"><SiSass color='#CC6699'/></li>
+                        <li className="Hero-li"><SiJavascript color='#F7DF1E'/></li>
+                        <li className="Hero-li"><SiTypescript color='#3178C6'/></li>
+                        <li className="Hero-li"><SiReact color='#61DAFB'/></li>
+                        <li className="Hero-li"><SiVite color='#646CFF'/></li>
+                        <li className="Hero-li"><SiNodedotjs color='#5FA04E'/></li>
+                        <li className="Hero-li"><FaJava color='#E34F26'/></li>
+                        <li className="Hero-li"><SiKotlin color='#7F52FF'/></li>
+                        <li className="Hero-li"><SiAndroidstudio color='#3DDC84'/></li>
+                        <li className="Hero-li"><SiPostgresql color='#4169E1'/></li>
+                        <li className="Hero-li"><SiFirebase color='#DD2C00'/></li>
+                    </ul>
+                </div>
+                <div className='Hero-down'>
+                    <div className="Hero-avatar">
+                        <img className="Hero-img" src="/public/imgs/programer_transp.svg" alt="svg avatar" />
+                    </div>
+                    <li className="Hero-li Hero-iconSocials Hero-iconSocials--first"><FaGithub color='#181717'/></li>
+                    <li className="Hero-li Hero-iconSocials Hero-iconSocials--second"><FaLinkedin color='#0D597F'/></li>
+                    <li className="Hero-li Hero-iconSocials Hero-iconSocials--last"><MdOutlineWeb color='#FFD800'/></li>
+                    <ul className="Hero-socials">
+                    </ul>
+                </div>
+            </div>
+        </section>
+    );
+}
