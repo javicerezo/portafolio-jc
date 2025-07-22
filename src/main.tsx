42a80f933e4661018ceb038db5dc8bdab9ae719c
Javi Cerezo <jc.webmob@gmail.com>
2025-07-22 12:06:17 +0200

    Cambios sin importancia

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './styles/main.scss'
-// este index contiene la carpeta styles al completo, reset de estilos de los navegadores, 
+// este main contiene la carpeta styles al completo, reset de estilos de los navegadores, 
 // configuraciones de preferencias del usuario, estilos globales a etiquetas...
 
 createRoot(document.getElementById('root')!).render(
