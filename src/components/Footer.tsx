df2070c40ac64ca85eeedbb129750f62bd740887
Javi Cerezo <jc.webmob@gmail.com>
2025-07-22 10:39:48 +0200

    Maquetar Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,22 @@
+import { useLanguage } from "../utils/context/useLanguage";
+
+import { FaGithub  } from "react-icons/fa";
+import { FaArrowTurnUp } from "react-icons/fa6";
+import { RiLinkedinFill } from "react-icons/ri";
 
 export const Footer = () => {
+    const { t } = useLanguage();
 
     return (
-        <p>Soy el footer</p>
+        <footer className="Footer">
+            <div className="Footer-name">
+                <h3>Javi Cerezo - {t.footer}</h3>
+            </div>
+            <ul className="Footer-ul">
+                <li className="Footer-li"><a href="https://github.com/javicerezo" target='_blank'><FaGithub color='#fff'/></a></li>
+                <li className="Footer-li"><a href="https://www.linkedin.com/in/javicerezo/" target='_blank'><RiLinkedinFill color='#fff'/></a></li>
+                <li className="Footer-li"><a href="#Hero"><FaArrowTurnUp color='#fff'/></a></li>
+            </ul>
+        </footer>
     );
 }
