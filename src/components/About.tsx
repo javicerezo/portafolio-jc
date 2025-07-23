eb04c7dbfd6615bd2d8695815ad058d4372f5d4c
Javi Cerezo <jc.webmob@gmail.com>
2025-07-23 14:39:42 +0200

    Maquetar sección about

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,7 +1,72 @@
+import { useLanguage } from "../utils/context/useLanguage";
 
 export const AboutMe = () => {
+    const { t } = useLanguage();
 
     return (
-        <section className="AboutMe">Soy la seccion de sobre mí. SIGUENTE TAREA</section>
+        <section className="About" id="About">
+            <div className="About-seccionTitle">
+                <h2>{t.about}:</h2>
+            </div>
+            <div className="About-container">
+                <div className="About-picture">
+                    <img className="About-img" src="/imgs/foto-perfil.jpg" alt="image me" />
+                    <ul className="About-credentialsList">
+                        <li className="About-credential">
+                            <div className="About-card">
+                                <div className="About-cardFront">
+                                    <span>Desarrollo web completo</span>
+                                </div>
+                                <div className="About-cardBack">
+                                    <img className="About-cardImg" src="/imgs/foto-perfil.jpg" alt="Universidad" />
+                                </div>
+                            </div>
+                        </li>
+                        <li className="About-credential">
+                            <div className="About-card">
+                                <div className="About-cardFront">
+                                    <span>Desarrollo web completo</span>
+                                </div>
+                                <div className="About-cardBack">
+                                    <img className="About-cardImg" src="/imgs/foto-perfil.jpg" alt="Universidad" />
+                                </div>
+                            </div>
+                        </li>
+                        <li className="About-credential">
+                            <div className="About-card">
+                                <div className="About-cardFront">
+                                    <span>Desarrollo web completo</span>
+                                </div>
+                                <div className="About-cardBack">
+                                    <img className="About-cardImg" src="/imgs/foto-perfil.jpg" alt="Universidad" />
+                                </div>
+                            </div>
+                        </li>
+                        <li className="About-credential">
+                            <div className="About-card">
+                                <div className="About-cardFront">
+                                    <span>Desarrollo web completo</span>
+                                </div>
+                                <div className="About-cardBack">
+                                    <img className="About-cardImg" src="/imgs/foto-perfil.jpg" alt="Universidad" />
+                                </div>
+                            </div>
+                        </li>
+
+
+
+                    </ul>
+                </div>
+                <ul className="About-description">
+                    <li className="About-paragraph">{t.about_1}</li>
+                    <li className="About-paragraph">{t.about_2}</li>
+                    <li className="About-paragraph">{t.about_3}</li>
+                    <li className="About-paragraph">{t.about_4}</li>
+                    <li className="About-paragraph">{t.about_5}</li>
+                    <li className="About-paragraph">{t.about_6}</li>
+                    <li className="About-paragraph">{t.about_7}</li>
+                </ul>
+            </div>
+        </section>
     );
 }
