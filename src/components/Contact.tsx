dba432d5d7129f48276b7d711e29abf95f28b4ac
Javi Cerezo <jc.webmob@gmail.com>
2025-07-23 14:44:46 +0200

    Crear las secciones contacto y proyectos (sin maquetar) para incorporación a nivel visual

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,9 +1,24 @@
+import { useLanguage } from "../utils/context/useLanguage";
 
 export const Contact = () => {
+    const { t } = useLanguage();
 
     return (
-        <section className="Contact">
-            Soy la seccion de contactos
+        <section className="Contact" id="Contact">
+            <div className="Contact-tituloSeccion">
+                <h2>{t.contact}:</h2>
+            </div>
+            <div className="Contact-container">
+                {/* <div className="About-picture">
+                    <img className="About-img" src="/imgs/foto-perfil.jpg" alt="image me" />
+                </div>
+                <div className="About-description">
+                    <div className="About-paragraph">Hola</div>
+                    <div className="About-paragraph">Hola</div>
+                    <div className="About-paragraph">Hola</div>
+                    <div className="About-paragraph">Hola</div>
+                </div> */}
+            </div>
         </section>
     );
 }
--- src/components/Proyect.tsx
+++ src/components/Proyect.tsx
@@ -1,9 +1,24 @@
+import { useLanguage } from "../utils/context/useLanguage";
 
 export const Proyect = () => {
+    const { t } = useLanguage();
 
     return (
-        <section className="Proyect">
-            Soy la seccion de proyectos
+        <section className="Proyect" id="Proyect">
+            <div className="Proyect-tituloSeccion">
+                <h2>{t.proyect}:</h2>
+            </div>
+            <div className="Proyect-container">
+                {/* <div className="About-picture">
+                    <img className="About-img" src="/imgs/foto-perfil.jpg" alt="image me" />
+                </div>
+                <div className="About-description">
+                    <div className="About-paragraph">Hola</div>
+                    <div className="About-paragraph">Hola</div>
+                    <div className="About-paragraph">Hola</div>
+                    <div className="About-paragraph">Hola</div>
+                </div> */}
+            </div>
         </section>
     );
 }
