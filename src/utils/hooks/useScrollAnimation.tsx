7db2cce50e75e6b1cae41d9d552736c5bce64302
Javi Cerezo <jc.webmob@gmail.com>
2025-08-03 14:27:56 +0200

    Crear hook para animación secciones al hacer scroll

---
+++ src/utils/hooks/useScrollAnimation.tsx
@@ -0,0 +1,25 @@
+import { useEffect, useState, useRef } from "react";
+
+export const useScrollAnimation = () => {
+    const ref = useRef<HTMLElement | null>(null);
+    const [ visible, setVisible ] = useState(false);
+
+    useEffect( () => {
+        const element = ref.current;
+        if(!element) return;
+
+        const observer = new IntersectionObserver( ([entry]) => {
+            if(entry.isIntersecting) {
+                setVisible(true);
+                observer.unobserve(element); // Solo se ejecuta una vez por elemento
+                }
+            },
+            { threshold: 0.2 } // se activa cuando el 20% del elemento es visible
+        );
+        observer.observe(element);
+
+        return () => observer.disconnect();
+    }, []);
+
+    return { ref, visible };
+}
