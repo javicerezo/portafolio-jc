3d1aa48a105083551ad3a839dcfeee9e847b7803
Javi Cerezo <jc.webmob@gmail.com>
2025-08-04 19:17:06 +0200

    Mover buttonConfig a carpeta components

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { LanguageProvider } from "./utils/context/LanguageProvider";
 
 import { Header } from "./components/Header";
 import { Hero } from "./components/Hero";
-import { ButtonConfig } from "./components/ButtonConfig/ButtonConfig";
+import { ButtonConfig } from "./components/ButtonConfig";
 import { Footer } from "./components/Footer";
 import { Contact } from "./components/Contact";
 import { Proyect } from "./components/Proyect/Proyect";
