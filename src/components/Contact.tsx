1af3c19b8b21c51dacc4fc6172a527b2d7b8ba62
Javi Cerezo <jc.webmob@gmail.com>
2025-08-06 11:52:33 +0200

    Añadir feedback al usuario según el estado del backend

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useLanguage } from "../utils/hooks/useLanguage";
 import { useScrollAnimation } from "../utils/hooks/useScrollAnimation";
 
@@ -10,6 +10,9 @@ export const Contact = () => {
     const { ref, visible } = useScrollAnimation();
 
     const [ status, setStatus ] = useState<"idle" | "sending" | "success" | "error">("idle");
+    const [ feedback, setFeedback ] = useState<string[]>([]);
+    const messageRefs = useRef<(HTMLParagraphElement | null)[]>([]);
+
 
     const sleep = (delay: number) => {
         return new Promise<void>((resolve) => setTimeout(resolve, delay));
@@ -23,41 +26,58 @@ export const Contact = () => {
 
         const formData = new FormData(e.currentTarget);
 
-        // validación honey
+        // Validación para evitar bots
         if(formData.get("company")) {
-        setStatus("success") // No enviamos nada, success para que el bot no haga de nuevo el formulario
-        return;
+            setStatus("success") // No enviamos datos, success para que el bot no haga de nuevo el formulario
+            return;
         }
-        
-        // Delay para mostrar spinner
+
+        // Delay para mostrar spinner para efecto procesando
         await sleep(3000);
 
         const response = await fetch("/.netlify/functions/sendMail", {
-        method: "POST",
-        body: JSON.stringify(Object.fromEntries(formData)),
-        })
+            method: "POST",
+            body: JSON.stringify(Object.fromEntries(formData)),
+        });
 
-        if(response.ok) {
+        const result = await response.json().catch(() => ({}));
+
+        if(result.status === "success") {
             setStatus("success");
+            setFeedback(result.message);
             form.reset();
+        } else if ( result.status === "ignored") {  // Bot detectado
+            setStatus("success");
         } else {
             setStatus("error");
-        }  
+            setFeedback(result.message);
+        }
     };
 
     useEffect( () => {
         if(status === "success" || status === "error") {
-            const timer = setTimeout( () => {
+            const timer1 = setTimeout( () => {
+                messageRefs.current.forEach( element => {
+                    if (element) element.classList.add(`Contact-${status}--outAnimation`);
+                });
+            }, 6000);
+            const timer2 = setTimeout( () => {
                 setStatus("idle");
-            }, 5000);
-            return () => clearTimeout(timer);
+                setFeedback([]);
+                messageRefs.current = [];
+            }, 8000);
+
+            return () => { 
+                clearTimeout(timer1); 
+                clearTimeout(timer2);
+            }
         }
 
     }, [status]);
 
     return (
-        <section 
-            className={`Contact sectionEffect ${visible ? "sectionEffect--show" : ""}`} 
+        <section
+            className={`Contact sectionEffect ${visible ? "sectionEffect--show" : ""}`}
             id="Contact"
             ref={ref}
             >
@@ -69,19 +89,20 @@ export const Contact = () => {
                     <textarea className="Contact-textArea" name="message" id="message" placeholder={t.contact_message} required />
 
                     {/* Campo oculto para evitar bots */}
-                    <input 
+                    <input
                         type="text"
                         name="company"
                         style={{ display:"none" }}
                         tabIndex={-1}   // inhabilitar posible tabulación usuario
-                        autoComplete="off" 
+                        autoComplete="off"
                         />
 
                     <button className="Contact-button" disabled={status === "sending"} type="submit">{t.contact_button}</button>
 
                     {/* FEEDBACK USUARIO */}
                     <div className="Contact-divResponse">
-                        {status === "sending" && 
+                        {/* spinner */}
+                        {status === "sending" &&
                             <div className="Contact-spinner sk-chase">
                                 <div className="sk-chase-dot"></div>
                                 <div className="sk-chase-dot"></div>
@@ -91,8 +112,18 @@ export const Contact = () => {
                                 <div className="sk-chase-dot"></div>
                             </div>
                         }
-                        {status === "success" && <p className="Contact-success">{t.contact_succes}</p>}
-                        {status === "error" && <p className="Contact-error">{t.contact_error}</p>}
+                        {/* mensaje de feedback */}
+                        {(status === "success" || status === "error") && (
+                            <div >
+                                {feedback.map( ( message, i ) => (
+                                    <p
+                                        key={i}
+                                        ref={ (element) => { messageRefs.current[i] = element }} 
+                                        className={status === "success" ? "Contact-success" : "Contact-error"}>{message}
+                                    </p> 
+                                ))}
+                            </div>
+                        )}
                     </div>
 
                 </form>
