1b88bbb2697226cf06f46dda78a130fa19ad6503
Javi Cerezo <jc.webmob@gmail.com>
2025-08-08 10:01:07 +0200

    Acoplar correctamente tooltip a CredentialCard

--- src/components/About/AboutCredentialCard.tsx
+++ src/components/About/AboutCredentialCard.tsx
@@ -14,28 +14,28 @@ export const AboutCredentialCard = ({ title, image, alt, href }: Props) => {
     const [ showTooltip, setShowTooltip ] = useState<boolean>(false);
     const { t } = useLanguage(); 
 
-    const handleClick = (e: React.MouseEvent<HTMLElement>) => {
-        e.preventDefault();
+    const handleClick = () => {
         setShowTooltip(true);
     }
 
     return (
         <li className="CredentialCard" >
-            <div className="CredentialCard-card" onClick={handleClick}>
+            <div className="CredentialCard-card" >
                 <div className="CredentialCard-cardFront">
                     <span>{title}</span>
                 </div>
-                <div className="CredentialCard-cardBack">
+                <div className="CredentialCard-cardBack" onClick={handleClick}>
                     <a href={href} target="_blank" rel="noopener noreferrer">
                         <img className="CredentialCard-cardImg" src={image} alt={alt} />
                     </a>
+                    {showTooltip && (
+                        <Tooltip
+                            text={t.tooltip_credentialcard} 
+                            onClose={() => setShowTooltip(false)}/>
+                    )}
                 </div>
             </div>
-            {showTooltip && (
-                <Tooltip
-                    text={t.tooltip_credentialcard} 
-                    onClose={() => setShowTooltip(false)}/>
-            )}
+            
         </li>
     );
 };
