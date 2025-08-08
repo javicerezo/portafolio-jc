9a478cbc0fc5bf0306dac6752e798cc080a7ca87
Javi Cerezo <jc.webmob@gmail.com>
2025-08-08 08:51:52 +0200

    Arreglar fallo darkMode en móviles en producción

--- src/utils/hooks/useDarkMode.ts
+++ src/utils/hooks/useDarkMode.ts
@@ -1,38 +1,5 @@
 import { useState, useEffect, useRef } from "react";
 
-// export const useDarkMode = () => {
-//     const [ darkModeON, setDarkModeON ] = useState( () => {
-//         // comprueba si existen preferencias en localStorage
-//         const saved = localStorage.getItem("theme");
-//         if (saved) return saved === "dark";
-
-//         // si no hay locaStorate, usa la preferencia del sistema
-//         if (window.matchMedia?.("(prefers-color-scheme: light)").matches) return false 
-        
-//         return true;
-//     });
-
-//     useEffect( () => {
-//         // Guarda en Storage 
-//         localStorage.setItem("theme", darkModeON ? "dark" : "light");
-        
-//         document.body.classList.toggle("lightMode", !darkModeON); 
-
-//         // Si tiene los contractes activados se desactivan
-//         if(document.body.classList.contains("highLightMode")) {
-//             document.body.classList.remove("highLightMode");
-//         }
-//         if(document.body.classList.contains("highDarkMode")) {
-//             document.body.classList.remove("highDarkMode");
-//         }
-//     }, [darkModeON]);
-
-//     const toogleDarkMode = () => setDarkModeON(!darkModeON);
-
-//     return [ darkModeON, toogleDarkMode ] as const;
-// };
-
-
 export const useDarkMode = () => {
     const [darkModeON, setDarkModeON] = useState<boolean>(true);
     const initialized = useRef(false);
