fc314e9a3ce3a3abb61d850c2e870ac12d5c4fc3
Javi Cerezo <jc.webmob@gmail.com>
2025-08-18 01:53:25 +0200

    Hacer los iconos de tecnologías con componente Icon

--- src/components/features/Hero.tsx
+++ src/components/features/Hero.tsx
@@ -1,9 +1,8 @@
 import { useLanguage } from '../../utils/hooks/useLanguage';
 
-import { SiHtml5, SiCss3, SiSass, SiJavascript, SiTypescript,
-         SiReact, SiVite, SiNodedotjs, SiPostgresql, SiFirebase, SiAndroidstudio,
-         SiKotlin } from 'react-icons/si';
-import { FaJava, FaGithub  } from "react-icons/fa";
+import { Icon } from '../ui/Icon';
+
+import { FaGithub  } from "react-icons/fa";
 import { RiLinkedinFill } from "react-icons/ri";
 
 export const Hero = () => {
@@ -23,19 +22,16 @@ export const Hero = () => {
                         </div>
                     </div>
                     <ul className="Hero-ul">
-                        <li className="Hero-li"><SiHtml5 color='#E34F26'/></li>
-                        <li className="Hero-li"><SiCss3 color='#663399'/></li>
-                        <li className="Hero-li"><SiSass color='#CC6699'/></li>
-                        <li className="Hero-li"><SiJavascript color='#F7DF1E'/></li>
-                        <li className="Hero-li"><SiTypescript color='#3178C6'/></li>
-                        <li className="Hero-li"><SiReact color='#61DAFB'/></li>
-                        <li className="Hero-li"><SiVite color='#646CFF'/></li>
-                        <li className="Hero-li"><SiNodedotjs color='#5FA04E'/></li>
-                        <li className="Hero-li"><FaJava color='#E34F26'/></li>
-                        <li className="Hero-li"><SiKotlin color='#7F52FF'/></li>
-                        <li className="Hero-li"><SiAndroidstudio color='#3DDC84'/></li>
-                        <li className="Hero-li"><SiPostgresql color='#4169E1'/></li>
-                        <li className="Hero-li"><SiFirebase color='#DD2C00'/></li>
+                        <Icon language="java" />
+                        <Icon language="kotlin" />
+                        <Icon language="react-native" />
+                        <Icon language="scss" />
+                        <Icon language="astro" />
+                        <Icon language="javascript" />
+                        <Icon language="typescript" />
+                        <Icon language="nodejs" />
+                        <Icon language="firebase" />
+                        <Icon language="postgresql" />
                     </ul>
                 </div>
                 <div className='Hero-down'>
