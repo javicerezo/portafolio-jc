b68faa27f5eb4bb111eae268836a65836dce0fd7
Javi Cerezo <jc.webmob@gmail.com>
2025-08-18 16:46:13 +0200

    Integrar nuevo componente en App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { LanguageProvider } from "./utils/context/LanguageProvider";
 import { Header } from "./components/features/Header";
 import { Hero } from "./components/features/Hero";
 import { ButtonConfig } from "./components/ui/ButtonConfig";
-import { LinesSVG } from "./components/ui/LinesSVG";
+import { LinesSVG } from "./components/ui/LinesSVG/LinesSVG";
 import { Proyect } from "./components/features/Proyect/Proyect";
 import { AboutMe } from "./components/features/About/About";
 import { Contact } from "./components/features/Contact";
