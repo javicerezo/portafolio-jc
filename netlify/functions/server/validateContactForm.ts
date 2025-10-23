dce86e589fff89678224f561b3c66acce446f2ee
Javi Cerezo <jc.webmob@gmail.com>
2025-10-23 10:57:26 +0200

    modificaciones de formato

--- netlify/functions/server/validateContactForm.ts
+++ netlify/functions/server/validateContactForm.ts
@@ -1,4 +1,4 @@
-import type { ContactFormData } from "../../../src/types/form";
+import type { ContactFormData } from "../../../src/utils/types/form";
 
 /**
  * Valida y limpia los datos de un formulario de contacto
